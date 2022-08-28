6d17f0ff677f8b4e3c448e2f10cbe220230de403
Samuel Mimram <smimram@gmail.com>
2022-08-28 12:49:57 +0200

    Indicate number of slides.

--- client.js
+++ client.js
@@ -125,7 +125,7 @@ function on_key(e) {
 function upload_slides() {
   data = document.querySelector('#slides-file').files[0];
   document.querySelector('#upload-slides').innerHTML = "<p>Uploading...</p>";
-  fetch(base_url() + "/upload", {method: "POST", body: data}).then((_) => document.querySelector('#upload-slides').innerHTML = "<p>Uploaded!</p>");
+  fetch(base_url() + "/upload", {method: "POST", body: data}).then((response) => response.text()).then((text) => document.querySelector('#upload-slides').innerHTML = '<p>Uploaded! ' + text + '</p>');
 }
 
 window.onload = function() {
