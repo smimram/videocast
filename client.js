d8499bf455caf5768a3bcb0d0552b3cfd70b7245
Samuel Mimram <smimram@gmail.com>
2022-09-01 16:39:23 +0200

    Nicer.

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 async function play() {
   console.log("Playing...");
+
   // window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
   // var audioContext = new AudioContext();
@@ -20,7 +21,9 @@ async function play() {
   */
 
   // Get video stream
-  const webcamId = document.getElementById('webcams').value;
+  const webcams = document.getElementById('webcams');
+  webcams.disabled = true;
+  const webcamId = webcams.value;
   const webcam = await navigator.mediaDevices.getUserMedia({
     audio: {
       echoCancellation: true,
@@ -33,15 +36,15 @@ async function play() {
   });
 
   // Display video
-  video = document.querySelector("video");
+  video = document.getElementById('video');
   video.srcObject = webcam;
 
   // Record video
-  const user = document.querySelector('#user').value;
-  const password = document.querySelector('#password').value;
-  const server = document.querySelector('#server').value;
-  const port = document.querySelector('#port').value;
-  const mount = document.querySelector('#mount').value;
+  const user = document.getElementById('user').value;
+  const password = document.getElementById('password').value;
+  const server = document.getElementById('server').value;
+  const port = document.getElementById('port').value;
+  const mount = document.getElementById('mount').value;
   const url = `ws://${user}:${password}@${server}:${port}/${mount}`;
 
   const bps = document.getElementById('kbps').value * 1000;
@@ -70,7 +73,7 @@ async function play() {
       }
     }
 
-    mediaRecorder.stop();
+    if (mediaRecorder.state != "inactive") { mediaRecorder.stop() }
     const ws = new Webcast.Socket({
       mediaRecorder,
       url: url,
@@ -85,12 +88,13 @@ async function play() {
   connect();
 
   function stop() {
+    webcams.disabled = false;
     mediaRecorder.stop();
     webcam.getTracks().forEach(track => track.stop());
     video.srcObject = null;
   }
 
-  document.querySelector('#stop').addEventListener('click', stop);
+  document.getElementById('stop').addEventListener('click', stop);
 }
 
 function base_url() {
@@ -100,7 +104,7 @@ function base_url() {
 }
 
 function refresh_slide () {
-  document.querySelector('#slide').src = base_url() + "/slide?" + new Date().getTime();
+  document.getElementById('slide').src = base_url() + "/slide?" + new Date().getTime();
 }
 
 async function prev() {
@@ -127,8 +131,8 @@ function on_key(e) {
 }
 
 function upload_slides() {
-  data = document.querySelector('#slides-file').files[0];
-  document.querySelector('#upload-slides').innerHTML = "<p>Uploading...</p>";
+  data = document.getElementById('slides-file').files[0];
+  document.getElementById('upload-slides').innerHTML = "<p>Uploading...</p>";
   fetch(base_url() + "/upload", {method: "POST", body: data}).then((response) => response.text()).then((text) => document.querySelector('#upload-slides').innerHTML = '<p>Uploaded! ' + text + '</p>');
 }
 
@@ -150,11 +154,11 @@ function enumerateWebcams() {
 }
 
 window.onload = function() {
-  document.querySelector('#start').addEventListener('click', play);
-  document.querySelector('#prev').addEventListener('click', prev);
-  document.querySelector('#next').addEventListener('click', next);
+  document.getElementById('start').addEventListener('click', play);
+  document.getElementById('prev').addEventListener('click', prev);
+  document.getElementById('next').addEventListener('click', next);
   refresh_slide();
   document.addEventListener('keydown', on_key);
-  document.querySelector('#upload-slides').addEventListener('submit', function(event) {event.preventDefault(); upload_slides()});
+  document.getElementById('upload-slides').addEventListener('submit', function(event) {event.preventDefault(); upload_slides()});
   enumerateWebcams();
 }
